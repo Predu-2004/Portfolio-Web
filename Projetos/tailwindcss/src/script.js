2116fd7106ab36230b2c82d5e8c39a422b3c433c
Pedro França <64810590+predufranca@users.noreply.github.com>
2022-12-02 18:14:07 -0300

    Delete Projetos/tailwindcss directory

--- Projetos/tailwindcss/src/script.js
+++
@@ -1,6 +0,0 @@
-btn = document.querySelector("button.mobile-menu-button");
-menu = document.querySelector(".mobile-menu");
-
-btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-});
